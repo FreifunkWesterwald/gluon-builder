9b29bfbd4ce8b8110be00a3a9feb23bddeb469a7
Fabian Cernota <fabian.cernota@chamaeleon.de>
2015-11-26 11:46:26 +0100

    Starting with Interactive Mode

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ Folgende Abhängigkeiten müssen gegeben sein:
 * [Abhängigkeiten von Gluon](http://gluon.readthedocs.org/en/v2015.1.1/user/getting_started.html#dependencies)
 * Man benötigt einen Dev-Zugang zum Imageserver (Zuständig: Fabian)
 * Einen Signkey
+
+
+
+## Wenn der Builder im GUI Mode gestartet werden soll sind noch folgende Abhängigkeiten vorhanden:
+
+* libnotify-bin
+* dialog
+
+
+## Starten:
+
+GUI Mode:
+./bash gui
+
+Servermode:
+./bash nogui ...
--- build.sh
+++ build.sh
@@ -3,90 +3,58 @@
 # Dieses Script ist der Einstiegspunkt für unsere Firmware
 #
 # Parameter:
-# $1 = Buildverzeichnis: Ein Verzeichnis in das alles ausgecheckt wird und die Firmware gebaut wird.
-# $2 = GLUON Version gegen die gebaut werden soll, z.B. v2015.1.2
-# $3 = BUILD ID Wird vom Jenkins übergeben
-# $4 = BRANCH
+# $2 Community z.B. Westerwald
+# $3 Netz z.b. Alt Neu
+# $4 Branch [stable | beta | experimental]
+# $5 build verzeichnis z.B. /home/gluon/build
 set -u
 set -e
 builderHome=`pwd`;
 
-echo "**********************";
-
-echo "FIRMWARE BUILDPROZESS GESTARTET";
-echo "FIRMWARE Builder in "  $builderHome  " gestartet";
-echo "Buildverzeichnis ist " $1;
-echo "Firmware wird für Gluon "$2" gebaut;"
-echo "Build ist: " $3;
-echo "Branch : " $4;
-export GLUON_BRANCH=$4
-export GLUON_RELEASE=build_$3;
-echo "**********************";
-cd $1
-pwd
-if [ -d gluon/ ] ; then
-  echo "verzeichnis vorhanden"
-  cd gluon
+if [ $1 == 'gui' ]
+  then
+   # Für welche Community soll gebaut werden?
+   community=`dialog --menu "Community auswählen" 0 0 0 \
+   "Westerwald" "" "Altenkirchen" "" 3>&1 1>&2 2>&3`
+   netz=`dialog --menu "Netz auswählen" 0 0 0 \
+   "Alt" "" "Neu" "" 3>&1 1>&2 2>&3`
+   branch=`dialog --menu "Branch auswählen" 0 0 0 \
+   "stable" "" "beta" "" "experimental" "" 3>&1 1>&2 2>&3`
+   verzeichnis=`dialog --inputbox "Wo soll die Firmware gebaut werden" 0 0 "/home/gluon/build" \
+ 3>&1 1>&2 2>&3`
+   targets=`dialog --checklist "Welche targets sollen gebaut werden? Jedes Target verbaucht 10 GB Platz!" 0 0 12 \
+ ar71xx-generic "" on\
+ ar71xx-nand "" off\
+ brcm2708-bcm2708 "" off\
+ brcm2708-bcm2709 "" off\
+ mpc85xx-generic "" off\
+ ramips-rt305x "" off\
+ x86-64 "" off\
+ x86-generic "" off\
+ x86-kvm_guest "" off\
+ x86-xen_domu "" off\
+ sunxi "" off 3>&1 1>&2 2>&3`
 else
-  git clone https://github.com/FreifunkWesterwald/gluon.git -b $2
-  cd gluon
+
+  community = $2
+  netz = $3
+  branch = $4
+  verzeichnis = $5
 fi
 
-if [ -d site/ ] ; then
-  echo "site vorhanden"
-  cd site
-  pwd
-  #git pull origin master
-  cd ..
-else
-  git clone https://github.com/FreifunkWesterwald/site-ffww.git site
+
+mkdir -p $verzeichnis
+git clone https://github.com/FreifunkWesterwald/gluon.git
+cd gluon/
+if [ community == 'Westerwald' ]
+  then
+    git clone https://github.com/FreifunkWesterwald/site-ffww.git site
+elif [[ community == 'Altenkirchen' ]];
+  then
+    git clone https://github.com/FreifunkWesterwald/site-ffak.git site
 fi
 
+cd ..
 make update
-echo "ar71xx-generic wird gebaut:"
-echo "==========================="
-export GLUON_TARGET=ar71xx-generic
-make -j2 V=s |& tee logfile
-echo "==========================="
-echo "ar71xx-nand wird gebaut:"
-echo "==========================="
-export GLUON_TARGET=ar71xx-nand
-make -j2 V=s |& tee logfile
-echo "==========================="
-echo "mpc85xx-generic wird gebaut:"
-echo "==========================="
-export GLUON_TARGET=mpc85xx-generic
-make -j2 V=s |& tee logfile
-echo "==========================="
-echo "x86-generic wird gebaut:"
-echo "==========================="
-export GLUON_TARGET=x86-generic
-make -j2 V=s |& tee logfile
-echo "==========================="
-echo "KVM Guest  wird gebaut:"
-echo "==========================="
-export GLUON_TARGET=x86-kvm_guest
-make -j2 V=s |& tee logfile
-echo "==========================="
 
-echo "Build fertig nun Manifest erstellen";
-make manifest
-echo "Checksummen generieren"
-cd images
-find "$PWD" -type d | sort | while read dir;
-do
-  cd "${dir}";
-  ls | grep -v "*/*" | egrep gluon- | egrep -v "(*md5|*sha1|*sha2)" | sort | while read file;
-  do
-    echo "Creating MD5 checksum for" $file "..."
-    md5sum $file > "$file.md5"
-    echo "Creating SHA1 checksum for" $file "..."
-    sha1sum $file > "$file.sha1"
-    echo "Creating SHA2 checksum for" $file "..."
-    sha512sum $file > "$file.sha2"
-  done
-done
-
-echo "==========================="
-echo "= FIRMWARE FERTIGGESTELLT ="
-echo "==========================="
+git clone https://github.com/FreifunkWesterwald/site-
